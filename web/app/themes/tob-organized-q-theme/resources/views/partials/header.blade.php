f6bded22d482aca7a92acc4326aeee280cc653e2
Alexis Guzman <alexis.guzman27@outlook.com>
2021-09-05 02:44:15 -0600

    Add mobile menu behavior

--- web/app/themes/tob-organized-q-theme/resources/views/partials/header.blade.php
+++ web/app/themes/tob-organized-q-theme/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="banner">
     <div class="container absolute">
         <nav class="nav-primary z-50 mt-6">
-            <div class="flex justify-between mx-7 md:mx-1">
+            <div class="flex justify-between mx-7 md:mx-1" x-data="{menuOpen:false}">
                 <div class="z-50 the-logo">
                     <a class="brand" href="{{ home_url('/') }}"
                     aria-label="{{ get_bloginfo('name', 'display') }}">
@@ -16,14 +16,29 @@
                         <img class="mobile-custom-logo" src="{{get_theme_mod( 'custom-mobile-logo' )}}" alt="Organized Q">
                     </a>
                 </div>
-                <button class="z-50 block md:hidden">
+                <button class="z-50 block md:hidden"
+                    x-on:click="menuOpen = !menuOpen"
+                >
                     <img class="" src="@php echo get_template_directory_uri(); @endphp/assets/images/icons/menu.svg" alt="menu">
                 </button>
-                <div class="z-50 hidden md:flex">
+                <div x-cloak :class="menuOpen ? 'z-50 h-screen md:flex bg-black top-0 left-0 absolute' : 'z-50 hidden md:flex'" style="z-index: 100;" x-transition>
+                    <div class="flex justify-between md:hidden my-6 px-10">
+                        <a class="brand" href="{{ home_url('/') }}"
+                            aria-label="{{ get_bloginfo('name', 'display') }}"
+                        >
+                            <img class="mobile-custom-logo" src="{{get_theme_mod( 'custom-mobile-logo' )}}" alt="Organized Q">
+                        </a>
+                        <button class="z-50 block md:hidden text-white  "
+                            x-on:click="menuOpen = false"
+                        >
+                            X
+                        </button>
+                    </div>
                     @if (has_nav_menu('primary_navigation'))
                         {!! wp_nav_menu(
                             [
-                                'theme_location' => 'primary_navigation', 'menu_class' => 'nav relative'
+                                'theme_location' => 'primary_navigation',
+                                'menu_class' => 'nav relative flex flex-col md:flex-row w-screen md:w-auto text-center'
                             ]
                         ) 
                         !!}
