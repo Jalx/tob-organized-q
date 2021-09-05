c43d1a33cf159235bc7d1d212e14a7aea54b0101
Alexis Guzman <alexis.guzman27@outlook.com>
2021-09-05 01:58:53 -0600

    Implement new custom mobile logo and add mobile menu button

--- web/app/themes/tob-organized-q-theme/resources/views/partials/header.blade.php
+++ web/app/themes/tob-organized-q-theme/resources/views/partials/header.blade.php
@@ -1,17 +1,24 @@
 <header class="banner">
     <div class="container absolute">
-        <nav class="nav-primary z-50">
-            <div class="flex justify-between">
-                <div class="z-50">
+        <nav class="nav-primary z-50 mt-6">
+            <div class="flex justify-between mx-7 md:mx-1">
+                <div class="z-50 the-logo">
                     <a class="brand" href="{{ home_url('/') }}"
                     aria-label="{{ get_bloginfo('name', 'display') }}">
-                        @php
-                        if ( function_exists( 'the_custom_logo' ) ) {
-                            the_custom_logo();
-                        }  
-                        @endphp
+                    @php
+                            if ( function_exists( 'the_custom_logo' ) ) {
+                                the_custom_logo();
+                            }
+                            @endphp
+                    </a>
+                    <a class="brand" href="{{ home_url('/') }}"
+                    aria-label="{{ get_bloginfo('name', 'display') }}">
+                        <img class="mobile-custom-logo" src="{{get_theme_mod( 'custom-mobile-logo' )}}" alt="Organized Q">
                     </a>
                 </div>
+                <button class="z-50 block md:hidden">
+                    <img class="" src="@php echo get_template_directory_uri(); @endphp/assets/images/icons/menu.svg" alt="menu">
+                </button>
                 <div class="z-50 hidden md:flex">
                     @if (has_nav_menu('primary_navigation'))
                         {!! wp_nav_menu(
@@ -31,10 +38,11 @@
         </div> --}}
         @if (get_header_image())
         <div id="site-header" class="site-header flex flex-col md:flex-row justify-end items-center w-full bg-front-page">
-            <div id="site-header-title" class="w-full md:w-5/12 flex flex-col text-white relative z-50 order-2 md:order-1">
-                <span class="">Virtual Executive Assistant services</span>
-                <span>small business owners and executives</span>
-                <h1>Welcome to Organized Q!</h1>
+            <div id="site-header-title" class="w-full md:w-5/12 flex flex-col text-white relative z-50 order-2 md:order-1 place-content-center">
+                <div class="text-center">
+                    <span class="uppercase">Virtual Executive Assistant services small business owners and executives</span>
+                    <h1 class="py-6">Welcome to Organized Q!</h1>
+                </div>
             </div>
             <img src="<?php header_image(); ?>"
             {{-- width="<?php echo absint( get_custom_header()->width ); ?>" --}}
