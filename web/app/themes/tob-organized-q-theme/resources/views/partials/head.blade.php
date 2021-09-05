b85342fe233520cf1ab786b4d50d8120d6d0e1ef
Alexis Guzman <alexis.guzman27@outlook.com>
2021-09-05 02:43:55 -0600

    Add alinejs

--- web/app/themes/tob-organized-q-theme/resources/views/partials/head.blade.php
+++ web/app/themes/tob-organized-q-theme/resources/views/partials/head.blade.php
@@ -3,4 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
