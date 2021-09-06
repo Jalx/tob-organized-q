a17d217a81963fa8d89d268e6d4b1d9b409cfa07
Alexis Guzman <alexis.guzman27@outlook.com>
2021-09-06 00:36:44 -0600

    Fix footer layour on xss screens

--- web/app/themes/tob-organized-q-theme/resources/views/partials/footer.blade.php
+++ web/app/themes/tob-organized-q-theme/resources/views/partials/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="content-info organized-q-footer">
     <div class="front-page-section">
         <div class="grid grid-cols-12">
-            <div class="col-start-2 col-span-10 md:col-span-4 mb-8">
+            <div class="col-start-2 col-span-10 md:col-start-2 md:col-span-10 xxl:col-span-4 mb-8">
                 <img class="organized-q-footer-logo mb-5" src="@php echo get_template_directory_uri(); @endphp/assets/images/icons/organized-q-logo.svg" alt="">
                 <h3 class="organized-q-footer-header">About Us</h3>
                 <p class="organized-q-footer-copy">In addition to helping individuals grow their own businesses, Organized Q is dedicated to providing flexible professional opportunities for other military spouses and family members who would like to work as virtual executive assistants while they support their military service member.</p>
             </div>
-            <div class="col-start-2 col-span-5 md:col-span-2">
+            <div class="col-start-2 col-span-5 md:col-start-2 md:col-span-5 xxl:col-span-2">
                 <h3 class="organized-q-footer-header">More Links</h3>
                 @if (has_nav_menu('primary_navigation'))
                     {!! wp_nav_menu(
@@ -18,7 +18,7 @@
                     !!}
                 @endif
             </div>
-            <div class="col-span-5 md:col-span-2 mb-12 mb:mb-12">
+            <div class="col-span-5 md:col-span-5 xxl:col-span-2 mb-12 mb:mb-12">
                 <h3 class="organized-q-footer-header">Services</h3>
                 @if (has_nav_menu('primary_navigation'))
                     {!! wp_nav_menu(
@@ -30,7 +30,7 @@
                     !!}
                 @endif
             </div>
-            <div class="col-start-2 col-span-5">
+            <div class="col-start-2 col-span-5 md:col-start-2 md:col-span-3 xxl:col-span-3">
                 <h3 class="organized-q-footer-header">Contact</h3>
                 <p class="organized-q-footer-copy">Washington, D.C.</p>
                 <div class="flex mb-6">
