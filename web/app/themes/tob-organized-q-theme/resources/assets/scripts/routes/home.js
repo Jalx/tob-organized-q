3f33fe7f986c94868f801ef7723c3925c9c5c366
Alexis Guzman <alexis.guzman27@outlook.com>
2021-09-06 00:38:11 -0600

    Implement intersection api for animation application on homepage

--- web/app/themes/tob-organized-q-theme/resources/assets/scripts/routes/home.js
+++ web/app/themes/tob-organized-q-theme/resources/assets/scripts/routes/home.js
@@ -1,8 +1,23 @@
 export default {
-  init() {
-    // JavaScript to be fired on the home page
-  },
-  finalize() {
-    // JavaScript to be fired on the home page, after the init JS
-  },
+    init() {
+        // JavaScript to be fired on the home page
+        var sections = document.querySelectorAll('.section');
+        const observer = new IntersectionObserver((sections) => {
+            sections.forEach(section => {
+                if (section.isIntersecting) {
+                    section.target.classList.add('section-animation');
+                } else {
+                    
+                    console.log(section);
+                    console.log('no');
+                }
+            });
+        });
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    },
+    finalize() {
+        // JavaScript to be fired on the home page, after the init JS
+    },
 };
